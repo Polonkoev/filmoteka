1b7d5749943b6654ee1e701251209132f19bae60
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-11-06 14:50:13 +0000

    Deploying to gh-pages from @ Polonkoev/filmoteka@09d78d9f93e2f6b6f90a97dfc299f954662ef4d9 🚀

---
+++ library.462e500b.js
@@ -0,0 +1,2 @@
+!function(){var o=document.getElementById("myBtn");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"}))}();
+//# sourceMappingURL=library.462e500b.js.map
