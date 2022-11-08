60b1d571f69535ab6b233db512e510c6aaccc082
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-11-08 06:52:24 +0000

    Deploying to gh-pages from @ Polonkoev/filmoteka@a4f0febcf49a38053440b96dc8e85705d8b3024e 🚀

--- library.368a418f.js
+++
@@ -1,2 +0,0 @@
-const o=document.getElementById("myBtn");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"}));
-//# sourceMappingURL=library.368a418f.js.map
