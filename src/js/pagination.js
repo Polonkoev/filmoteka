cf8eff0b4a35639237f092d02f85bafe55bf540a
Adilnurlan <nurlan.adil98@gmail.com>
2022-11-09 18:45:16 +0600

    commit1

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -8,14 +8,23 @@ const options = {
   itemsPerPage: 20,
   visiblePages: 5,
   centerAlign: true,
-  page: 1,
+  page: JSON.parse(localStorage.getItem('page')),
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
 };
+
+if (!localStorage.getItem('page')) {
+  options.page = 1;
+  const fetchApi = new ApiService({
+    page: 1,
+  });
+  fetchApi.fetchTrendMovies().then(handleSucces).catch(handleError);
+}
+
 const pagination = new Pagination(container, options);
 
 const fetchApi = new ApiService({
-  page: 1
+  page: JSON.parse(localStorage.getItem('page')),
 });
 fetchApi.fetchTrendMovies().then(handleSucces).catch(handleError);
 
@@ -29,11 +38,12 @@ function handleError(error) {
   console.error(error);
 }
 
-pagination.on('afterMove', (event) => {
+pagination.on('afterMove', event => {
   const galleryEl = document.querySelector('.movieList');
   galleryEl.innerHTML = '';
   const fetchApi = new ApiService({
     page: event.page,
   });
   fetchApi.fetchTrendMovies().then(handleSucces).catch(handleError);
-})
+  localStorage.setItem('page', JSON.stringify(event.page));
+});
